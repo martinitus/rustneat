009ecabf32fbb55c95677227b030cf16d191bb1f
Martin Rückl <enigma@nbubu.de>
2022-05-14 13:33:51 +0200

    more tests and split / insert logic

--- src/population.rs
+++ src/population.rs
@@ -3,7 +3,7 @@ use ndarray::prelude::*;
 use lazycell::LazyCell;
 use std::cmp::max;
 use petgraph::{Directed, Graph};
-use petgraph::graph::{EdgeReference, NodeIndex};
+use petgraph::graph::{EdgeReference, NodeIndex, Node, EdgeIndex};
 
 /// A connection Gene
 #[derive(Debug, Clone, PartialEq)]
@@ -57,19 +57,17 @@ impl Genome {
         let inputs: Vec<NodeIndex> = (0..inputs).map(|i| graph.add_node(())).collect();
         let outputs: Vec<NodeIndex> = (0..outputs).map(|i| graph.add_node(())).collect();
 
-        // let mut connections = Vec::default();
         let mut c = 0;
         for o in outputs.iter() {
             graph.add_edge(bias, *o, Connection { weight: 1., enabled: true, innovation_id: c });
-            // let conn = graph.edge_weight(i).unwrap();
-            // connections.push(conn);
+            c = c + 1;
             for i in inputs.iter() {
                 graph.add_edge(*i, *o, Connection { weight: 1., enabled: true, innovation_id: c });
                 c = c + 1;
             }
         }
 
-        Genome { graph, bias, inputs, outputs }//, connections }
+        Genome { graph, bias, inputs, outputs }
     }
 
     /// Vector of input node indices
@@ -85,6 +83,42 @@ impl Genome {
     pub fn connection(&self, source: NodeIndex, target: NodeIndex) -> Option<&'_ Connection> {
         assert_eq!(self.graph.edges_connecting(source, target).count(), 1);
         self.graph.edges_connecting(source, target).next().map(|er| er.weight())
+    }
+
+    /// Insert a new connection between the given two nodes.
+    ///  - There must not already be a connection.
+    ///  - Passed innovation id must be greater than any innovation id in this genome.
+    pub fn insert(&mut self, source: NodeIndex, target: NodeIndex, innovation_id: usize) -> EdgeIndex {
+        assert!(innovation_id > self.graph.edge_weights().last().unwrap().innovation_id);
+        assert_eq!(self.graph.edges_connecting(source, target).count(), 0);
+        // fixme: random weight?
+        self.graph.add_edge(source, target, Connection { enabled: true, weight: 0., innovation_id })
+    }
+
+    /// Split an existing connection into two by inserting a new node and disabling the present connection.
+    /// - There must be a connection from source to target
+    /// - The connection from source to target must be enabled
+    pub fn split(&mut self, source: NodeIndex, target: NodeIndex, innovation_id: usize) -> (EdgeIndex, EdgeIndex) {
+        assert_eq!(self.graph.edges_connecting(source, target).count(), 1);
+        assert_eq!(self.graph.edges_connecting(source, target).next().unwrap().weight().enabled, true);
+        let old = self.graph.edges_connecting_mut(source, target).next().unwrap().weight();
+        old.enabled = false;
+
+        let node = self.graph.add_node(());
+
+        let e1 = self.graph.add_edge(source, node, Connection {
+            weight: 0.0, // fixme: weight
+            enabled: true,
+            innovation_id: innovation_id + 0,
+        });
+
+        let e2 = self.graph.add_edge(source, node, Connection {
+            weight: 0.0, // fixme: weight
+            enabled: true,
+            innovation_id: innovation_id + 1,
+        });
+
+        (e1, e2)
     }
 
     /// Get the connection with given innovation number.
@@ -108,32 +142,64 @@ impl Genome {
         self.graph.edge_weights()
             .filter(move |&gene| gene.innovation_id > threshold)
     }
-    pub fn disjoint(&self, other: &Genome) -> impl Iterator<Item=&'_ Connection> {
-        unimplemented!();
-        self.graph.edge_weights()
+
+    pub fn disjoint<'a>(&'a self, other: &'a Genome) -> impl Iterator<Item=&'a Connection> {
+        let threshold = other.graph.edge_weights().last().unwrap().innovation_id;
+        self.mismatching(other)
+            .filter(move |&gene| gene.innovation_id < threshold)
     }
-    pub fn matching<'o>(&'o self, other: &'o Genome) -> impl Iterator<Item=(&'o Connection, &'o Connection)> {
+
+    /// An iterator over connections that are in self, but not in other.
+    pub fn mismatching<'a>(&'a self, other: &'a Genome) -> impl Iterator<Item=&'a Connection> {
         Gn::new_scoped(move |mut s| {
-            let mut it1 = self.graph.edge_weights().into_iter();
-            let mut it2 = other.graph.edge_weights().into_iter();
-            while true {
-                let e1 = it1.next();
-                let e2 = it2.next();
-                if e1.is_none() || e2.is_none() {
-                    break;
+            for e1 in self.graph.edge_weights() {
+                // fixme eventually performance optimize this
+                let mut found = false;
+                for e2 in other.graph.edge_weights() {
+                    if e1.innovation_id == e2.innovation_id {
+                        found = true;
+                        break;
+                    }
                 }
-                let e1 = e1.unwrap();
-                let e2 = e2.unwrap();
-                if e1.innovation_id == e2.innovation_id {
-                    s.yield_((e1, e2));
-                }
-                if e1.innovation_id >= e2.innovation_id {
-                    e2 = it2.next();
-                } else {
-                    e1 = it1.next();
+                if !found {
+                    s.yield_(e1);
                 }
             }
             done!();
+        })
+    }
+
+    /// An iterator over the aligned connections of the two genomes. The first element of the tuples
+    /// will be the connection of self, the second element the connection of other.
+    pub fn matching<'o>(&'o self, other: &'o Genome) -> impl Iterator<Item=(&'o Connection, &'o Connection)> {
+        Gn::new_scoped(move |mut s| {
+            for e1 in self.graph.edge_weights() {
+                for e2 in other.graph.edge_weights() {
+                    if e1.innovation_id == e2.innovation_id {
+                        s.yield_((e1, e2));
+                    }
+                }
+            }
+            done!();
+            // fixme eventually performance optimize this
+            // while true {
+            //     let e1 = it1.next();
+            //     let e2 = it2.next();
+            //     if e1.is_none() || e2.is_none() {
+            //         break;
+            //     }
+            //     let e1 = e1.unwrap();
+            //     let e2 = e2.unwrap();
+            //     if e1.innovation_id == e2.innovation_id {
+            //         s.yield_((e1, e2));
+            //     }
+            //     if e1.innovation_id >= e2.innovation_id {
+            //         e2 = it2.next();
+            //     } else {
+            //         e1 = it1.next();
+            //     }
+            // }
+            // done!();
         })
     }
 
@@ -173,7 +239,6 @@ impl Species {}
 //     ChangeWeight(),
 //     ToggleExpression(),
 // }
-
 
 const MAX_EPOCHS_WITHOUT_IMPROVEMENTS: usize = 10;
 
@@ -294,16 +359,74 @@ mod tests {
     use super::*;
 
     #[test]
-    fn new_population_should_contain_n_fully_connected_genomes() {
-        let p = Population::new(10, 3, 3);
-        assert_eq!(p.genomes().len(), 10);
-        let sample = &p.genomes()[0];
-        for g in p.genomes() {
-            assert_eq!(g, sample);
+    fn new_genome_should_be_fully_connected() {
+        let g = Genome::new(3, 3);
+        assert_eq!(g.graph.edge_count(), 3 * 3 + 3);
+
+        assert!(g.connection(g.bias, g.outputs[0]).is_some());
+        for i in 0..3 {
+            assert!(g.connection(g.inputs[0], g.outputs[0]).is_some());
+        }
+    }
+
+    #[test]
+    fn two_new_new_genomes_should_be_fully_aligned() {
+        let g1 = Genome::new(3, 3);
+        let g2 = Genome::new(3, 3);
+
+        let overlap1: Vec<(&Connection, &Connection)> = g1.matching(&g2).collect();
+        let overlap2: Vec<(&Connection, &Connection)> = g2.matching(&g1).collect();
+
+        for e in &overlap1 {
+            println!("{:?}", &e);
+        }
+        assert_eq!(overlap1.len(), 12);
+        assert_eq!(overlap2.len(), 12);
+        assert_eq!(overlap1, overlap2);
+
+        let excess1: Vec<&Connection> = g1.excess(&g2).collect();
+        let disjoint1: Vec<&Connection> = g1.disjoint(&g2).collect();
+
+        let excess2: Vec<&Connection> = g2.excess(&g1).collect();
+        let disjoint2: Vec<&Connection> = g2.disjoint(&g1).collect();
+
+        assert_eq!(excess1.len(), 0);
+        assert_eq!(disjoint1.len(), 0);
+        assert_eq!(excess2.len(), 0);
+        assert_eq!(disjoint2.len(), 0);
+    }
+
+    #[test]
+    fn genome_matching_with_one_excess() {
+        /// Create a graph that has one extra hidden node between
+        /// bias and output0 nodes.
+        fn initial_genome() -> Genome {
+            let mut g1 = Genome::new(2, 2);
+            let edges = g1.split(g1.bias, g1.outputs[0], 12);
+            g1
         }
 
-        assert_eq!(sample.graph.edge_count(), 3 * 3 + 3)
-        // fixme: add test for connectivity?
+        let mut g1 = initial_genome();
+        let mut g2 = initial_genome();
+        g2.insert()
+
+        let overlap1: Vec<(&Connection, &Connection)> = g1.matching(&g2).collect();
+        let overlap2: Vec<(&Connection, &Connection)> = g2.matching(&g1).collect();
+
+        assert_eq!(overlap1.len(), 12);
+        assert_eq!(overlap2.len(), 12);
+        assert_eq!(overlap1, overlap2);
+
+        let excess1: Vec<&Connection> = g1.excess(&g2).collect();
+        let disjoint1: Vec<&Connection> = g1.disjoint(&g2).collect();
+
+        let excess2: Vec<&Connection> = g2.excess(&g1).collect();
+        let disjoint2: Vec<&Connection> = g2.disjoint(&g1).collect();
+
+        assert_eq!(excess1.len(), 2);
+        assert_eq!(disjoint1.len(), 0);
+        assert_eq!(excess2.len(), 0);
+        assert_eq!(disjoint2.len(), 0);
     }
 
     #[test]
